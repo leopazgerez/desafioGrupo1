41f69f8ca30390ba9175cad3262c98197b36df10
Leonel Paz Gerez <104148448+leopazgerez@users.noreply.github.com>
2022-10-05 22:10:17 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,14 +29,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _toggled = false;
+  late Toggle toggle;
+  bool _toggled4 = false;
   bool _toggled1 = false;
   bool _toggled2 = false;
   bool _toggled3 = false;
-  _setToggle(bool value){
-    setState(() {
-      _toggled = value;
-    });
+  _setToggle(bool value) {
+    switch (toggle) {
+      case Toggle.one:
+        return setState(() {
+          _toggled1 = value;
+        });
+    case Toggle.two:
+        return setState(() {
+          _toggled2 = value;
+        });
+    case Toggle.three:
+        return setState(() {
+          _toggled3 = value;
+        });
+    case Toggle.four:
+        return setState(() {
+          _toggled4 = value;
+        });
+    }
   }
 
   @override
@@ -49,42 +65,38 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: <Widget>[
             SwitchListTile(
-                value: _toggled,
-                onChanged: (bool value) {
-                  setState(() {
-                    _toggled = value;
-                  });
-                },
+              value: _toggled1,
+              onChanged: _setToggle,
               title: const Text('Toggle'),
               secondary: const Icon(Icons.ac_unit),
             ),
             SwitchListTile(
-                value: _toggled1,
-                onChanged: (bool value) {
-                  setState(() {
-                    _toggled1 = value;
-                  });
-                },
+              value: _toggled2,
+              onChanged: (bool value) {
+                setState(() {
+                  _toggled2 = value;
+                });
+              },
               title: const Text('Toggle 1'),
               secondary: const Icon(Icons.add_alert_outlined),
             ),
             SwitchListTile(
-                value: _toggled2,
-                onChanged: (bool value) {
-                  setState(() {
-                    _toggled2 = value;
-                  });
-                },
+              value: _toggled3,
+              onChanged: (bool value) {
+                setState(() {
+                  _toggled3 = value;
+                });
+              },
               title: const Text('Toggle 2'),
               secondary: const Icon(Icons.adb),
             ),
             SwitchListTile(
-                value: _toggled3,
-                onChanged: (bool value) {
-                  setState(() {
-                    _toggled3 = value;
-                  });
-                },
+              value: _toggled4,
+              onChanged: (bool value) {
+                setState(() {
+                  _toggled4 = value;
+                });
+              },
               title: const Text('Toggle 3'),
               secondary: const Icon(Icons.add_chart),
             ),
@@ -94,9 +106,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-enum Toggle{
-  toggle,
-  toggle1,
-  toggle2,
-  toggle3
-}
+
+enum Toggle { one, two, three, four }
